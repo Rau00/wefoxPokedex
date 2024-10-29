a4f5288cc37481337aeecb15d3dcc5924982f83a
Ragnorak-dev <raulillangarcia@gmail.com>
2024-10-29 13:04:31 +0100

    Removed not used class

--- app/src/main/java/technical/test/pokedex/data/datasources/remote/network/model/ResultData.kt
+++
@@ -1,8 +0,0 @@
-package technical.test.pokedex.data.datasources.remote.network.model
-
-sealed class ResultData<out T : Any> {
-
-    class Success<out T : Any>(val data: T) : ResultData<T>()
-
-    class Error(val exception: Throwable) : ResultData<Nothing>()
-}
